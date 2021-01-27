4f178eef40c67add55421ba279ea4924d5c383f1
SiThuHeinn <72207337+SiThuHeinn@users.noreply.github.com>
2021-01-27 17:33:37 +0630

    updated
    winner check bug fixed

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ bool placeMarker(int slot)
 
 int winner()
 {
-    for (int i = 0; i < 2 ; i++){
+    for (int i = 0; i < 3 ; i++){
 
         //rows
         if(board[i][0] == board[i][1] && board[i][1]  == board[i][2]) return current_player;
@@ -81,12 +81,28 @@ void switch_player_marker()
 }
 
 void clearGameBoard(){
-    int slotNumbers = 0;
-    for(auto & i : board){
-        for (char & j : i) {
-            slotNumbers++;
-            char slot = static_cast<char>(slotNumbers);
-            j = slot;
+     for(int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; ++j) {
+
+            if( i == 0 && j == 0){
+                board[0][0] = '1';
+            }else if( i == 0 && j == 1 ){
+                board[0][1] = '2';
+            }else if(i == 0 && j == 2){
+                board[0][2] = '3';
+            }else if( i == 1 && j == 0){
+                board[1][0] = '4';
+            }else if( i == 1 && j == 1){
+                board[1][1] = '5';
+            }else if ( i == 1 && j == 2){
+                board[1][2] = '6';
+            }else if ( i == 2 && j == 0){
+                board[2][0] = '7';
+            }else if (i ==2 && j == 1){
+                board[2][1] = '8';
+            }else if(i == 2 && j == 2){
+                board[2][2] = '9';
+            }
         }
     }
 }
